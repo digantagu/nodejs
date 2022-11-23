daaddf2540935fcb6a81bed66836292b0cd54cc2
Diganta <digantagu@gmail.com>
2022-11-23 14:05:22 +0530

    Create sonar-project.js

---
+++ sonar-project.js
@@ -0,0 +1,17 @@
+const sonarqubeScanner = require( 'sonarqube-scanner');
+sonarqubeScanner({
+serverUrl: 'http://192.168.10.215:9000/',
+options : {
+'sonar.project Description': 'This is a Node JS application
+'sonar.projectName': 'Node JS Application Sample',
+'sonar.projectKey': 'NodeJsMSS',
+'sonar. login': 'admin',
+'sonar.password': 'abc123',
+'sonar.projectVersion': '1.0',
+'sonar. language': 'js',
+'sonar. sourceEncoding': 'UTF-8',
+'sonar. sources': '.',
+//'sonar.tests': 'specs',
+//'sonar.inclusions': 'src/***
+},
+}, () => {});
