9d5104e2cd53c36ea96712212d74017e5f3ea140
Diganta <digantagu@gmail.com>
2022-11-24 23:18:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World! This is running with CI/CD pipeline')
+  response.send('Hello Neeraj! This is running with CI/CD pipeline')
 })
 
 app.listen(app.get('port'), function() {
