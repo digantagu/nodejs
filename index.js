5e6313093462093ece07fd9eb77bc2d83dff5f51
Diganta <digantagu@gmail.com>
2022-11-25 12:25:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('If you see this then it is ok!!!')
+  response.send('Hello Diganta...How r u ??')
 })
 
 app.listen(app.get('port'), function() {
