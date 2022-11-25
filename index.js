c4b6c814486d7d399800c89ad0d274f16eb4ebf9
Diganta <digantagu@gmail.com>
2022-11-25 13:28:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic')
+  response.send('Lorem Ipsum is simply dummy text of the printing and typesetting industry.')
 })
 
 app.listen(app.get('port'), function() {
