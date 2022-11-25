9e1b2937e6bb89925fe841a8830b9b043805202c
Diganta <digantagu@gmail.com>
2022-11-25 10:27:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Neeraj! This is running with CI/CD pipeline')
+  response.send('Lachit Barphukan was a commander and Borphukan, in the Ahom kingdom, located in present-day Assam, India, known for his leadership in the 1671 Battle of Saraighat that thwarted a drawn-out attempt by Mughal forces under the command of Ramsingh I to take over Ahom kingdom. He died about a year later due to illness.')
 })
 
 app.listen(app.get('port'), function() {
