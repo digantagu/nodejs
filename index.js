b537673abd7c5a54465194b72b959345dce2e3f9
Diganta <digantagu@gmail.com>
2022-11-25 14:01:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-var express = require('express')
-var app = express()
-
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('GUWAHATI IS A CAPITAL OF ASSAM')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+// Import essential libraries 
+const express = require('express'); 
+const app = express(); 
+const path = require('path'); 
+const router = express.Router(); 
+// Setup essential routes 
+router.get('/', function(req, res) { 
+    res.sendFile(path.join(__dirname + '/index.html')); 
+    //__dirname : It will resolve to your project folder. 
+}); 
+router.get('/about', function(req, res) { 
+    res.sendFile(path.join(__dirname + '/about.html')); 
+}); 
+router.get('/sitemap', function(req, res) { 
+    res.sendFile(path.join(__dirname + '/sitemap.html')); 
+}); 
+//add the router 
+app.use('/', router); 
+app.listen(process.env.port || 5000); 
+console.log('Running at Port 5000'); 
